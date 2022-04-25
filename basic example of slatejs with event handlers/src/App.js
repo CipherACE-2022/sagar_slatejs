e68e0b69a3d4cecb7eef3f98e28effdd50ffaeac
SAGAR J <75424678+sagarjgb@users.noreply.github.com>
2022-04-25 18:36:14 +0530

    Add files via upload

---
+++ basic example of slatejs with event handlers/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Editor from './components/slateEdi';
+
+function App() {
+  return (
+    <div className="App">
+     <Editor />
+    </div>
+  );
+}
+
+export default App;
