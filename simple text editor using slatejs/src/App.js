dbb6a366fdc2ff3c478cf8f78c92e597df3ea52a
SAGAR J <75424678+sagarjgb@users.noreply.github.com>
2022-04-27 19:29:34 +0530

    Create App.js

---
+++ simple text editor using slatejs/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Edi from './components/slateEdi';
+
+function App() {
+  return (
+    <div className="App">
+     <Edi />
+    </div>
+  );
+}
+
+export default App;
