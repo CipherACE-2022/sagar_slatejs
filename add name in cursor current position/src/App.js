68901ca913aebfb00a0d53b1e59bb44b704956f2
SAGAR J <75424678+sagarjgb@users.noreply.github.com>
2022-05-01 12:29:18 +0530

    Create App.js

---
+++ add name in cursor current position/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Cname from './components/custName';
+
+function App() {
+  return (
+    <div className="App">
+      <Cname /> 
+    </div>
+  );
+}
+
+export default App;
